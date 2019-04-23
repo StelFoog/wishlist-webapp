64a1f6311c456b7cbd96957a2d70b62fcaa715eb
Axel Pettersson <axel@pettersson.cc>
2019-04-23 15:40:44 +0200

    First version of buttons

--- src/features/components/button/Button.js
+++ src/features/components/button/Button.js
@@ -1,1 +1,24 @@
 import React from "react";
+
+const Button = ({ variant = "filled", color, label, handleClick }) => (
+  <div className="button-container">
+    {variant == "filled" && (
+      <button
+        style={{ background: color, color: "#fff" }}
+        onClick={handleClick}
+      >
+        {label}
+      </button>
+    )}
+    {variant == "outlined" && (
+      <button
+        style={{ color: color, background: rgba(255, 255, 255, 0.3) }}
+        onClick={handleClick}
+      >
+        {label}
+      </button>
+    )}
+  </div>
+);
+
+export default Button;
