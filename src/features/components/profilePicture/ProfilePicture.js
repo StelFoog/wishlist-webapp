74e36fc06f1bd08a185d71fe6af946f2309313d4
Christopher Gustafson <christopher.gustafson@outlook.com>
2019-04-25 14:36:21 +0200

    Added profile picture component

---
+++ src/features/components/profilePicture/ProfilePicture.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import "./profilePicture.css";
+
+
+const ProfilePicture = ({
+  src,
+  width = 100
+}) => (
+    <img
+      className="profilePicture"
+      src={src}
+      style={{ width: width }}
+    ></img>
+  );
+
+
+
+export default ProfilePicture;
