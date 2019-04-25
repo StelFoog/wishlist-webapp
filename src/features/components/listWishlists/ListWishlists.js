8e934beef96bdb4fc924854a4aa24c34656a0575
Christopher Gustafson <christopher.gustafson@outlook.com>
2019-04-25 16:08:42 +0200

    Tweaked list wishlists component

--- src/features/components/listWishlists/ListWishlists.js
+++ src/features/components/listWishlists/ListWishlists.js
@@ -1,32 +1,34 @@
 import React, { Component } from 'react';
 import PageHeader from "../pageHeader";
 import CardContainer from "../card/CardContainer";
-import CardHeader from "../card/cardHeader";
-import CardContent from "../card/cardContent";
+import { CardContent, CardHeader } from "../card/";
 
 import "./listWishlists.css";
 
 class ListWishlists extends Component {
+
+  getWishlists = () => {
+    return (
+
+      <CardContainer children={
+        <div className="cardContent">
+          <CardHeader children={"Wishlist"} />
+          <CardContent children={
+            <p>Nulla esse quis velit officia reprehenderit
+              reprehenderit consequat.</p>
+          } />
+        </div>
+      } />
+    );
+  }
+
   render() {
     return (
       <div className="listWishlists">
         <PageHeader title={"Your wishlists"} />
 
-
-        <CardContainer children={
-          <div className="cardContent">
-            <CardHeader children={"Wishlist"} />
-            <CardContent children={
-              <p>Nulla esse quis velit officia reprehenderit
-                reprehenderit consequat.</p>
-            } />
-          </div>
-        } />
-
-
-
-
-
+        {/* Fetch users wishlists from database*/}
+        {this.getWishlists()}
 
       </div>
     );
