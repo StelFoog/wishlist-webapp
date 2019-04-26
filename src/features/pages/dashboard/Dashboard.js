05cbdb8af0cd5afb0ae27b2daa877e26d5c11ac0
Christopher Gustafson <christopher.gustafson@outlook.com>
2019-04-26 11:09:04 +0200

    Added create wishlist button to dashboard

--- src/features/pages/dashboard/Dashboard.js
+++ src/features/pages/dashboard/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import DashboardNav from "../../components/dashboardNav";
 import ListWishlists from "../../components/listWishlists";
+import Button from "../../components/button";
 import "./dashboard.css";
 
 class Dashboard extends Component {
@@ -8,7 +9,13 @@ class Dashboard extends Component {
     return (
       <div className="dashboard">
         <DashboardNav />
+
+        { /* Here we place whatever dashboard page user is on, currently listWishlists as placeholder */}
         <ListWishlists />
+        <div className="createWishlistButton">
+          <Button variant={"filled"} label={"+"} color={"var(--color-secondary)"} />
+        </div>
+
       </div>
     );
   }
