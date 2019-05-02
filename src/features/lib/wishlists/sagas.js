fbb15b89b636a0fb4bc053d889d1f73cc75e94fb
Christopher Gustafson <christopher.gustafson@outlook.com>
2019-05-02 16:57:32 +0200

    Added functions to fetch wishlists

--- src/features/lib/wishlists/sagas.js
+++ src/features/lib/wishlists/sagas.js
@@ -1,6 +1,7 @@
 import { takeEvery, call, put, select, all } from "redux-saga/effects";
 import { getFormValues, reset } from "redux-form";
 import db from "./db";
+import { createWishlistWithOwner, fetchAllWishlistsFromUser } from "./db.js";
 import { getUser } from "../authentication/selectors";
 import { addNewWishlistIdToUser } from "../authentication/db";
 import wishlistTypes from "./types.js";
@@ -12,7 +13,10 @@ const { createWishlistWithOwner } = db;
 const {
   CREATE_USER_WISHLIST,
   CREATE_USER_WISHLIST_ERROR,
-  CREATE_USER_WISHLIST_SUCCESS
+  CREATE_USER_WISHLIST_SUCCESS,
+  FETCH_WISHLISTS,
+  FETCH_WISHLISTS_ERROR,
+  FETCH_WISHLISTS_SUCCESS
 } = wishlistTypes;
 
 const { ADD_WISHLIST_ID_TO_USER } = authTypes;
@@ -47,4 +51,21 @@ function* workCreateUserWishlist() {
   }
 }
 
-export default { watchCreateUserWishlist };
+function* watchFetchWishlists() {
+  yield takeEvery(FETCH_WISHLISTS, workFetchWishlists);
+}
+
+function* workFetchWishlists() {
+  try {
+    const user = yield call(getUser);
+    const wishlists = yield call(fetchAllWishlistsFromUser(user.uid));
+    yield put({
+      type: FETCH_WISHLISTS_SUCCESS,
+      wishlistData: wishlists
+    });
+  } catch (error) {
+    yield put({ type: FETCH_WISHLISTS_ERROR, error: error });
+  }
+}
+
+export default { watchCreateUserWishlist, watchFetchWishlists };
