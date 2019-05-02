b1b25c61b25d5666b4cab1a1f25c3776aaa80868
jfjell <47498768+jfjell@users.noreply.github.com>
2019-05-02 15:17:51 +0200

    Added dashboard with placeholder information

--- src/features/components/listWishlists/ListWishlists.js
+++ src/features/components/listWishlists/ListWishlists.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import PageHeader from "../pageHeader";
 import CardContainer from "../card/CardContainer";
 import { CardContent, CardHeader } from "../card/";
@@ -12,14 +12,6 @@ import { ConnectedRouter } from "connected-react-router";
 import Dashboard from "../../pages/dashboard";
 import HomePage from "../../pages/homePage";
 
-function WishlistPageTest({match}) {
-  return <h1> {"Hello" + match.params.uid } </h1>;
-}
-
-export { WishlistPageTest }
-
-const { fetchWishlists } = actions;
-
 const MAX_WISHLIST_AVATARS = 5;
 
 function homog_seq(x, n) {
@@ -30,38 +22,21 @@ function homog_seq(x, n) {
   return seq;
 }
 
-function createWishlistRoutes() {
-  return(
-    <ConnectedRouter history="none">
-      <Router>
-        <Switch>
-          <Route
-            path={"/dashboard/wishlist/test"}
-            exact
-            render={(props) => {
-              return <HomePage {...props} />
-            }}
-          />
-          <Route
-            path={"/dashboard/wishlist/test2"}
-            exact
-            render={(props) => {
-              return <Dashboard {...props} />
-            }}
-          />
-        </Switch>
-      </Router>
-    </ConnectedRouter>
-  );
-}
-
 function getUserAvatarElem(user) {
   return (
-    <ProfilePicture src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png" width="30px" />
+    <ProfilePicture
+      src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png"
+      width="30px"
+    />
   );
 }
 
-const moreMembers = <ProfilePicture src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png" width="30px" />;
+const moreMembers = (
+  <ProfilePicture
+    src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png"
+    width="30px"
+  />
+);
 
 function getWishlistAvatars(wishlist) {
   const members = homog_seq(1, 10); // placeholder
@@ -70,29 +45,35 @@ function getWishlistAvatars(wishlist) {
     .map(getUserAvatarElem);
 
   if (members.length >= MAX_WISHLIST_AVATARS)
-    avatars.push(members.length === MAX_WISHLIST_AVATARS
-      ? getUserAvatarElem(members[MAX_WISHLIST_AVATARS - 1])
-      : moreMembers);
+    avatars.push(
+      members.length === MAX_WISHLIST_AVATARS
+        ? getUserAvatarElem(members[MAX_WISHLIST_AVATARS - 1])
+        : moreMembers
+    );
   return avatars;
 }
 
 class ListWishlists extends Component {
   displayWishlist(wishlist) {
     return (
-      <CardContainer children={
-        <div className="cardContent">
-          <CardHeader children={wishlist.title} />
-          <hr />
-          <CardContent children={
-            <div className="wishlistCard">
-              <p className="wishlistText">...</p>
-              <div className="wishlistAvatarList">
-                {getWishlistAvatars(wishlist)}
-              </div>
-            </div>
-          } />
-        </div>
-      } />
+      <CardContainer
+        children={
+          <div className="cardContent">
+            <CardHeader children={wishlist.title} />
+            <hr />
+            <CardContent
+              children={
+                <div className="wishlistCard">
+                  <p className="wishlistText">...</p>
+                  <div className="wishlistAvatarList">
+                    {getWishlistAvatars(wishlist)}
+                  </div>
+                </div>
+              }
+            />
+          </div>
+        }
+      />
     );
   }
 
@@ -103,19 +84,19 @@ class ListWishlists extends Component {
   getWishlists() {
     //const x = { title: "Yellow", text: "blue red black yellow green gray orange purple magenta violet turqoise lime white blue yellow orange brown gray" };
     console.log(this.props);
-    return this.props.wishlists.map(this.displayWishlist)
+    return this.props.wishlists.map(this.displayWishlist);
   }
 
   render() {
     return (
-    <div>
-      <Route path="/dashboard/nirb" exact component={Dashboard} />
-      <div className="listWishlists">
-        <PageHeader title={"Your wishlists"} />
-        {/* Fetch users wishlists from database*/}
-        {this.getWishlists()}
+      <div>
+        <Route path="/dashboard/nirb" exact component={Dashboard} />
+        <div className="listWishlists">
+          <PageHeader title={"Your wishlists"} />
+          {/* Fetch users wishlists from database*/}
+          {this.getWishlists()}
+        </div>
       </div>
-    </div>
     );
   }
 }
@@ -124,12 +105,12 @@ const mapStateToProps = () => {
   const getWishlists = selectors.getWishlistsState();
   return state => ({
     wishlists: getWishlists(state)
-  })
-}
+  });
+};
 
 const mapDispatchToProps = dispatch => ({
   fetchWishlists: () => dispatch(fetchWishlists())
-})
+});
 
 export default connect(
   mapStateToProps,
