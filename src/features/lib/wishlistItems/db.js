54409bf032a9f35fc3b510eb3f5d27855e48e9b9
jfjell <47498768+jfjell@users.noreply.github.com>
2019-05-06 10:27:32 +0200

    Added remove/edit item functions for wishlists

--- src/features/lib/wishlistItems/db.js
+++ src/features/lib/wishlistItems/db.js
@@ -2,10 +2,26 @@ import { db as wishlistDb } from "../wishlists";
 
 const { fetchWishlistByUid, _getWishlistRef } = wishlistDb;
 
-const addWishlistItem = (uid, item) => {
-  let wishlist = fetchWishlistByUid(uid);
+const addWishlistItem = async (uid, item) => {
+  let wishlist = await fetchWishlistByUid(uid);
   wishlist.items.push(item);
   _getWishlistRef(uid).set(wishlist);
 };
 
-export { addWishlistItem };
+async function editWishlistItem(uid, index, item) {
+  let wishlist = await fetchWishlistByUid(uid);
+  if(index < 0 || index >= wishlist.items.length)
+    throw new Error("editWishlistItem(): Item index out of bounds");
+  wishlist.items[index] = item;
+  _getWishlistRef(uid).set(wishlist);
+}
+
+async function removeWishlistItem(uid, index) {
+  let wishlist = await fetchWishlistByUid(uid);
+  if(index < 0 || index >= wishlist.items.length)
+    throw new Error("removeWishlistItem(): Item index out of bounds");
+  wishlist.items.splice(index, 1);
+  _getWishlistRef(uid).set(wishlist);
+}
+
+export { addWishlistItem, editWishlistItem, removeWishlistItem };
