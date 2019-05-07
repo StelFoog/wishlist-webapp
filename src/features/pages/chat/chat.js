ec782a701a8952772145eed05c73710f602af51f
jfjell <47498768+jfjell@users.noreply.github.com>
2019-05-07 15:32:49 +0200

    Added date formatting (bugged)

--- src/features/pages/chat/chat.js
+++ src/features/pages/chat/chat.js
@@ -19,6 +19,21 @@ const createChatClick = (props) => {
   }
 }
 
+const formatTimestamp = (timestamp) => {
+  let time = new Date(0);
+  time.setUTCSeconds(timestamp.seconds)
+  return time.getHours() 
+       + ":" 
+       + time.getMinutes() 
+       + ", " 
+       + time.getFullYear() 
+       + "/" 
+       + (time.getMonth() + 1)
+       + "/" 
+       + (time.getDay() + 5)
+  ;
+}
+
 class Chat extends Component {
   componentDidMount() {
     onChatMessageReceived(0, ((props) => {
