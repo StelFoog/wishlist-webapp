c3c44cfe8ad3a75bd6c3fc1a2cf096d8e87fa458
samuel <samuel@familjenlarsson.eu>
2019-05-09 15:32:10 +0200

    added boilerplate for redux, selectors and sagas

---
+++ src/boilerplate/types.js
@@ -0,0 +1,16 @@
+// Just the types that are used in relation to the component or lib they belong
+// to. Think Elixir atoms.
+
+const BOILER_FOO = "BOILER_FOO";
+const BOILER_BAR = "BOILER_BAR";
+const BOILER_THING = "BOILER_THING";
+const BOILER_THING_SUCCESS = "BOILER_THING_SUCCESS";
+const BOILER_THING_ERROR = "BOILER_THING_ERROR";
+
+export default {
+  BOILER_FOO,
+  BOILER_BAR,
+  BOILER_THING,
+  BOILER_THING_SUCCESS,
+  BOILER_THING_ERROR
+};
