2daeb1e8decf58adacddf25c484dcf772d58fd22
Axel Pettersson <axel@pettersson.cc>
2019-05-10 16:51:06 +0200

    Initial group page

---
+++ src/features/pages/groupPage/GroupPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class GroupPage extends React.Component {
+  render() {
+    const { uid } = this.props.match.params;
+    return <div>hej</div>;
+  }
+}
+
+export default GroupPage;
