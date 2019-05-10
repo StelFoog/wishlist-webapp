d66072c7f903b1be11192cfc30431b8e34aaefc1
Axel Pettersson <axel@pettersson.cc>
2019-05-10 11:07:34 +0200

    Simplified the chatbubble component

--- src/features/pages/invitedWishlistPage/components/chatBubble/ChatBubble.js
+++ src/features/pages/invitedWishlistPage/components/chatBubble/ChatBubble.js
@@ -4,43 +4,26 @@ import ProfilePicture from "../../../../components/profilePicture";
 
 const ChatBubble = ({
   username,
+  sameSender = false,
   userProfilePicture,
   messageText,
   sent = false
 }) => {
-  if (sent) {
-    return (
-      <React.Fragment>
-        <div className="messageContent messageContentSent">
-          <div className="imageContainer">
-            <ProfilePicture width={40} src={userProfilePicture} />
-          </div>
-          <div className="chatNameContiner">
-            <span>{username}</span>
-            <div className="chatBubble chatBubbleSent">
-              <p>{messageText}</p>
-            </div>
-          </div>
+  let text = messageText.split(/(?:\r\n|\r|\n)/g);
+  return (
+    <div className={`messageContent ${sent ? "messageContentSent" : ""}`}>
+      <div className={`imageContainer ${sameSender ? "noPicture" : ""}`}>
+        {!sameSender && <ProfilePicture width={40} src={userProfilePicture} />}
+      </div>
+      <div className="chatNameContiner">
+        {!sameSender && <span>{username}</span>}
+        <div className={`chatBubble ${sent ? "chatBubbleSent" : ""}`}>
+          {text.map(value => (
+            <p key={`${value} ${username}`}>{value}</p>
+          ))}
         </div>
-      </React.Fragment>
-    );
-  } else {
-    return (
-      <React.Fragment>
-        <div className="messageContent">
-          <div className="imageContainer">
-            <ProfilePicture width={40} src={userProfilePicture} />
-          </div>
-          <div className="chatNameContiner">
-            <span>{username}</span>
-            <div className="chatBubble">
-              <p>{messageText}</p>
-            </div>
-          </div>
-        </div>
-      </React.Fragment>
-    );
-  }
+      </div>
+    </div>
+  );
 };
-
 export default ChatBubble;
