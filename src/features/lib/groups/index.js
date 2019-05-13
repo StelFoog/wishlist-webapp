c43d989a12fe7bc41ff2894134935cc9cc5d244e
Axel Pettersson <axel@pettersson.cc>
2019-05-13 09:17:16 +0200

    Backend fixes for groups

--- src/features/lib/groups/index.js
+++ src/features/lib/groups/index.js
@@ -2,6 +2,7 @@ import actions from "./actions";
 import db from "./db";
 import reducers from "./reducers";
 import sagas from "./sagas";
+import selectors from "./selectors";
 import types from "./types";
 
-export { actions, db, reducers, sagas, types };
+export { actions, db, reducers, sagas, types, selectors };
