91b73dba7962a575e4f0ef24e1d372ca4cbd34e1
Axel Pettersson <axel@pettersson.cc>
2019-05-13 09:18:10 +0200

    Fetch group members

--- src/features/pages/groupPage/GroupPage.js
+++ src/features/pages/groupPage/GroupPage.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { MemberList } from "./components";
 
 class GroupPage extends React.Component {
   render() {
     const { uid } = this.props.match.params;
-    return <div>hej</div>;
+    const { groups } = this.props;
+    const group = groups.find(el => el.uid === uid);
+
+    return (
+      <div>
+        <MemberList members={group.members} />
+      </div>
+    );
   }
 }
 
