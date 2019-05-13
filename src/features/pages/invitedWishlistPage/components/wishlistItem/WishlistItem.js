763211734c22373a4a95cc3d02078a476543333a
Christopher Gustafson <christopher.gustafson@outlook.com>
2019-05-13 14:33:03 +0200

    Changed layout and added claim functionality to items at invited wishlists

--- src/features/pages/invitedWishlistPage/components/wishlistItem/WishlistItem.js
+++ src/features/pages/invitedWishlistPage/components/wishlistItem/WishlistItem.js
@@ -1,31 +1,83 @@
 import React from "react";
 import { connect } from "react-redux";
-import CardContainer, {
-  CardContent,
-  CardHeader
-} from "../../../../components/card";
+import Button from "../../../../components/button";
 import { selectors } from "../../../../lib/wishlists";
+import { actions } from "../../../../lib/wishlistItems"
+import "./wishlistItem.css";
+import ProfilePicture from "../../../../components/profilePicture/ProfilePicture";
 
-const wishListItem = ({ wishlists, index, editItem, wishlistUid }) => {
+const { claimWishlistItem } = actions;
+
+const wishListItem = ({ wishlists, index, wishlistUid, claimItem }) => {
   const wishlist = wishlists.find(element => element.uid == wishlistUid);
   const item = wishlist.items[index];
   console.log(item);
-  const { name, description, price } = item;
-  return (
-    <CardContainer key={name}>
-      <div className="itemContent">
-        <div className="itemText">
-          <CardHeader children={name} />
-          <CardContent>
-            <p>{description}</p>
-          </CardContent>
-        </div>
+  const { name, description, price, claimedBy } = item;
 
-        <div className="itemPrice">
-          <h3>{price}:-</h3>
+  function getClaimContent() {
+    if (claimedBy === undefined || claimedBy.length == 0) {
+      return (
+        <div className="itemContent itemClaim">
+          <Button
+            handleClick={() => claimItem(wishlistUid, index)}
+            variant="filled"
+            label="Claim item"
+            color="var(--color-primary)"
+          />
         </div>
+      );
+    }
+    else {
+      return (
+        <div className="itemContent itemClaim">
+          <h3>Claimed by:</h3>
+          <div className="claimUsers">
+            {claimedBy.map(uid => getUserWhoClaimed(uid))}
+          </div>
+        </div>
+      );
+    }
+  }
+
+
+  function getUserWhoClaimed(uid) {
+
+    return (
+      <div className="userWhoClaimed">
+        {/*
+        <div className="userWhoClaimedName">
+          <p>{name}</p>
+        </div>
+      */}
       </div>
-    </CardContainer>
+
+    );
+  }
+
+  return (
+
+    <React.Fragment>
+      <div className="wishlistItem">
+        <div className="itemContent itemTitle"><h2>{name}</h2></div>
+        <div className="itemContent itemDescription">
+          <p>{description}</p>
+        </div>
+        <div className="itemContent">
+          <div className="itemPrice"><h3>{price}:-</h3></div>
+          <div className="itemLink">
+            <Button
+              variant="filled"
+              label="Link"
+              className="itemLinkButton"
+              padding="0"
+              color="var(--color-primary)"
+            />
+          </div>
+        </div>
+        {getClaimContent()}
+      </div>
+
+    </React.Fragment>
   );
 };
 
@@ -36,7 +88,11 @@ const mapStateToProps = () => {
   });
 };
 
+const mapDispatchToProps = dispatch => ({
+  claimItem: (wishlistUid, index) => dispatch(claimWishlistItem(wishlistUid, index))
+});
+
 export default connect(
   mapStateToProps,
-  null
+  mapDispatchToProps
 )(wishListItem);
