4099772afc528f1b192d6b9bce34fad5821c8054
Christopher Gustafson <christopher.gustafson@outlook.com>
2019-05-16 09:31:09 +0200

    Added selector to fetch currently logged in user

--- src/features/lib/authentication/index.js
+++ src/features/lib/authentication/index.js
@@ -2,5 +2,6 @@ import actions from "./actions";
 import reducers from "./reducers";
 import sagas from "./sagas";
 import types from "./types";
+import selectors from "./selectors";
 
-export { actions, reducers, sagas, types };
+export { actions, reducers, sagas, types, selectors };
--- src/features/lib/authentication/selectors.js
+++ src/features/lib/authentication/selectors.js
@@ -1,1 +1,11 @@
+import { createSelector } from "reselect";
+
 export const getUser = ({ auth }) => auth.user;
+
+const getCurrentUserState = () =>
+  createSelector(
+    [getUser],
+    currentUser => currentUser
+  );
+
+export default { getCurrentUserState };
