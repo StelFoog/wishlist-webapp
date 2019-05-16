22980a1986695e3e020738fad6e1779ce4349e2d
samuel <samuel@familjenlarsson.eu>
2019-05-16 16:49:08 +0200

    created getter for router...search

--- src/features/lib/router/selectors.js
+++ src/features/lib/router/selectors.js
@@ -1,6 +1,7 @@
 import { createSelector } from "reselect";
 
 export const getPathname = ({ router }) => router.location.pathname;
+export const getSearch = ({ router }) => router.location.search;
 
 const getPathhameState = () =>
   createSelector([getPathname], pathname => pathname);
