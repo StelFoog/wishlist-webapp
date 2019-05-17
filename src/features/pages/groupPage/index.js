66177e616d41676f2e939cc3639b5626862e6db7
Johannes Borg <johannes.borg@hotmail.com>
2019-05-17 10:25:54 +0200

    Implements group view DB listening

--- src/features/pages/groupPage/index.js
+++ src/features/pages/groupPage/index.js
@@ -2,14 +2,23 @@ import { connect } from "react-redux";
 import GroupPage from "./GroupPage";
 import { selectors as groupSelectors } from "../../lib/groups";
 
+import { actions } from "../../lib/groups/";
+
+const { updateCurrentGroup } = actions;
+
 const mapStateToProps = () => {
   const getGroups = groupSelectors.getGroupsState();
   return state => ({
-    groups: getGroups(state)
+    groups: getGroups(state),
+    user: state.auth
   });
 };
 
+const mapDispatchToProps = dispatch => ({
+  updateCurrentGroup: group => dispatch(updateCurrentGroup(group))
+});
+
 export default connect(
   mapStateToProps,
-  null
+  mapDispatchToProps
 )(GroupPage);
