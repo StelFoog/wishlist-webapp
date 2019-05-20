19b7b90f026a63af0f5bbc0ab2f4ef23a17db9db
Axel Pettersson <axel@pettersson.cc>
2019-05-20 12:21:08 +0200

    New sharing dialog

--- src/features/components/shareForm/UserCard.js
+++ src/features/components/shareForm/UserCard.js
@@ -1,39 +1,35 @@
-import React from "react";
+import React, { Fragment } from "react";
 import CardContainer from "../card/CardContainer";
 import { CardContent } from "../card";
 import ProfilePicture from "../profilePicture/ProfilePicture.js";
 import Button from "../button";
-import Paper from "../paper";
 import "./userCard.css";
 
-const UserCard = ({user, buttonText, buttonColor, onClick}) => {
-  return(
-    <Paper>
-    { !user ? "Loading..." :
-      <div className="userCard">
-        <div className="userAvatar">
-          <ProfilePicture
-            src={user.profilePictureUrl}
-            width="50px"
-          />
+const UserCard = ({ user, buttonText, buttonColor, onClick }) => {
+  return (
+    <React.Fragment>
+      {!user ? (
+        "Loading..."
+      ) : (
+        <div className="userCard">
+          <div className="userAvatar">
+            <ProfilePicture src={user.profilePictureUrl} width="50px" />
+          </div>
+          <div className="username">{user.name}</div>
+          <div className="addOrRemoveButton">
+            <Button
+              fontSize="1rem"
+              padding="0.5rem 1rem"
+              variant="text"
+              label={buttonText}
+              handleClick={onClick}
+              color={buttonColor}
+            />
+          </div>
         </div>
-        <div className="username">
-          {user.name}
-        </div>
-        <div className="addOrRemoveButton">
-          <Button
-            fontSize="1rem"
-            padding="0.5rem 1rem"
-            variant="filled"
-            label={buttonText}
-            handleClick={onClick}
-            color={buttonColor}
-          />
-        </div>
-      </div>
-    }
-    </Paper>
+      )}
+    </React.Fragment>
   );
-}
+};
 
 export default UserCard;
