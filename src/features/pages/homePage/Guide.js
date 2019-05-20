99d4e89ed0ebe8f8ff4dd26967035a9ed38e6014
samuel <samuel@familjenlarsson.eu>
2019-05-20 10:51:10 +0200

    Added proper text to the frontpage-guide

--- src/features/pages/homePage/Guide.js
+++ src/features/pages/homePage/Guide.js
@@ -18,8 +18,9 @@ const Guide = () => (
           <CardMedia />
           <CardContent>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-              et ex luctus, condimentum nisi id,
+              Create a new Wishlist, give it a name and, if you want to, give it
+              a description and set a time for when the gift-giving event will
+              happen.
             </p>
           </CardContent>
         </Card>
@@ -33,10 +34,7 @@ const Guide = () => (
         <Card elevation={2}>
           <CardMedia />
           <CardContent>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-              et ex luctus, condimentum nisi id,
-            </p>
+            <p>Add items you want to the wishlist...</p>
           </CardContent>
         </Card>
       </div>
@@ -50,8 +48,8 @@ const Guide = () => (
           <CardMedia />
           <CardContent>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-              et ex luctus, condimentum nisi id,
+              And then share it with your fiends and family or anyone else who
+              might need to know what you wish for.
             </p>
           </CardContent>
         </Card>
@@ -65,9 +63,35 @@ const Guide = () => (
         <Card elevation={2}>
           <CardMedia />
           <CardContent>
+            <p>Other people can see what you want...</p>
+          </CardContent>
+        </Card>
+      </div>
+      <div className="guide-col">
+        <div className="guide-step">
+          <Paper elevation={2} className="circle">
+            <span>5</span>
+          </Paper>
+        </div>
+        <Card elevation={2}>
+          <CardMedia />
+          <CardContent>
+            <p>Discuss who should get what...</p>
+          </CardContent>
+        </Card>
+      </div>
+      <div className="guide-col">
+        <div className="guide-step">
+          <Paper elevation={2} className="circle">
+            <span>6</span>
+          </Paper>
+        </div>
+        <Card elevation={2}>
+          <CardMedia />
+          <CardContent>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-              et ex luctus, condimentum nisi id,
+              And claim items from your wishlist, so everyone you've invited can
+              see what someone is already getting you.
             </p>
           </CardContent>
         </Card>
