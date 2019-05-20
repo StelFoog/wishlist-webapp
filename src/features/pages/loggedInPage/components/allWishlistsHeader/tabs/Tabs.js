19cbcbd7b677d3a62e435ffa1472b00af3313ffa
Axel Pettersson <axel@pettersson.cc>
2019-05-20 15:50:06 +0200

    Animated tab bar indicator

--- src/features/pages/loggedInPage/components/allWishlistsHeader/tabs/Tabs.js
+++ src/features/pages/loggedInPage/components/allWishlistsHeader/tabs/Tabs.js
@@ -2,11 +2,6 @@ import React from "react";
 
 import "./tabs.css";
 
-class Tabs extends React.Component {
-  render() {
-    const { /* fullWidth = false, */ children } = this.props;
-    return <div className="tabs">{children}</div>;
-  }
-}
+const Tabs = ({ children }) => <div className="tabs">{children}</div>;
 
 export default Tabs;
