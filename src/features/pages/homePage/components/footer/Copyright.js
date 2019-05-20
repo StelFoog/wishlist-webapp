9f553b002982a9084db1f266fb067fbda4ff62f2
samuel <samuel@familjenlarsson.eu>
2019-05-20 09:29:47 +0200

    updated copyright

--- src/features/pages/homePage/components/footer/Copyright.js
+++ src/features/pages/homePage/components/footer/Copyright.js
@@ -1,5 +1,5 @@
 import React from "react";
 
-const Copyright = () => <span>Wishlist ©2019</span>;
+const Copyright = () => <span>Wishlist © 2019 Team Arpanet-19</span>;
 
 export default Copyright;
