599d9c44bfdb19deeb2c42d00009585afee84084
samuel <samuel@familjenlarsson.eu>
2019-05-20 09:32:39 +0200

    redid footer legal links

--- src/features/pages/homePage/components/footer/Navigation.js
+++ src/features/pages/homePage/components/footer/Navigation.js
@@ -1,27 +1,17 @@
 import React from "react";
+import "./footer.css";
 
 const Navigation = () => (
   <div className="footer-navigation">
-    <ul className="footer-app-links">
-      <li>
-        <a href="">Home</a>
-      </li>
-      <li>
-        <a href="">Dashboard</a>
-      </li>
-      <li>
-        <a href="">Något annat</a>
-      </li>
-    </ul>
     <ul className="footer-legal-links">
       <li>
-        <a href="">User policy</a>
+        <a href=""> User policy </a>
       </li>
       <li>
-        <a href="">Annan policy</a>
+        <a href=""> Annan policy </a>
       </li>
       <li>
-        <a href="">Cookies</a>
+        <a href=""> Cookies </a>
       </li>
     </ul>
   </div>
