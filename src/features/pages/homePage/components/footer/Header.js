6bee1f9cd4b00490e41614200ec75c4a6a674c41
samuel <samuel@familjenlarsson.eu>
2019-05-20 09:33:02 +0200

    redid footer header

--- src/features/pages/homePage/components/footer/Header.js
+++ src/features/pages/homePage/components/footer/Header.js
@@ -3,8 +3,13 @@ import logo from "../../../../../logo.svg";
 
 const Header = () => (
   <div className="footer-header">
-    <h3>Wishlist</h3>
-    <img src={logo} alt="footer logo" />
+    <div className="footer-logo">
+      <h3>Wishlist</h3>
+      <img src={logo} alt="footer logo" />
+    </div>
+    <div className="footer-to-top">
+      <a onClick={() => window.scrollTo(0, 0)}>Back to Top</a>
+    </div>
   </div>
 );
 
