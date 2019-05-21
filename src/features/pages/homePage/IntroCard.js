b9805c2bd1bb35b521ac103bffec5f2ff9bb8f15
Oscar <oscarmey@kth.se>
2019-05-21 14:01:57 +0200

    Fixed some spelling on the homepage

--- src/features/pages/homePage/IntroCard.js
+++ src/features/pages/homePage/IntroCard.js
@@ -14,12 +14,12 @@ const IntroCard = () => (
         </h3>
         <p>
           It's easy! Make a list, add all the items you want to it and share it
-          with people. Everyone you shared it with can then plan for what gifts
-          they are to give.
+          with friends and family. Everyone you shared it with can then plan for
+          what gifts they are to give.
           <br />
           <br />
-          For an event or other ocation where any people are all going to give
-          eachother gifts there are <b>groups</b>. Everyone in a group has a
+          For an event or other occasions where everybody will be giving each
+          other gifts, there are <b>groups</b>. Everyone in a group has a
           wishlist specific for the group that everyone else in the group can
           see and claim items in.
         </p>
