51d0dc2b9db17ecfb69977b75d792eec74581e56
jfjell <47498768+jfjell@users.noreply.github.com>
2019-05-21 16:44:24 +0200

    Fixed http(s) being broken for group items

--- src/features/lib/groupItems/db.js
+++ src/features/lib/groupItems/db.js
@@ -1,12 +1,19 @@
 import { firebase } from "../firebase";
 import { _getGroupRef } from "../groups/db";
 
+const validateLink = (link) => {
+  return link.replace("https://", "")
+             .replace("http://", "");
+}
+
 const addGroupWishlistItem = async ({ groupID, userID, itemData }) => {
+  itemData.weblink = validateLink(itemData.weblink);
   await _getGroupRef(groupID).update({
     ["wishlists." + userID]: firebase.firestore.FieldValue.arrayUnion(itemData)
   });
 };
 const editGroupWishlistItem = async ({ groupID, userID, itemData, index }) => {
+  itemData.weblink = validateLink(itemData.weblink);
   let doc = await fetchGroupWishlistItems({ groupID, userID });
   doc[index] = itemData;
   await _getGroupRef(groupID).update({
